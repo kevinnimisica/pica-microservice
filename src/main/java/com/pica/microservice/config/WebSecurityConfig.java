f7d2dc786a9fcd1ce17137966588a9afbaaa2387
rbenprieto <rbenprieto@outlook.com>
2024-11-12 23:00:03 -0500

    allow validate

--- src/main/java/com/pica/microservice/config/WebSecurityConfig.java
+++ src/main/java/com/pica/microservice/config/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig {
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth ->
 										   auth.requestMatchers("/api/auth/**").permitAll()
+											   .requestMatchers("/api/auth/validate").permitAll()
 											   .requestMatchers("/api/test/**").permitAll()
 											   .anyRequest().authenticated()
 								  );
